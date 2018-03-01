68a61ea59b9a46ff65911312058f4a039971565a
Srdjan Pejic <srdjan.pejic@shopify.com>
2018-03-01 17:02:15 -0500

    Instead of overwriting credentials and regions, simply append

--- bin/install_awscli.sh
+++ bin/install_awscli.sh
@@ -8,13 +8,13 @@ export PATH=~/vendor/awscli/bin:$PATH
 
 mkdir -p ~/.aws
 
-cat >> ~/.aws/credentials << EOF
+cat > ~/.aws/credentials << EOF
 [default]
 aws_access_key_id = $AWS_KEY
 aws_secret_access_key = $AWS_SECRET_KEY
 EOF
 
-cat >> ~/.aws/config << EOF
+cat > ~/.aws/config << EOF
 [default]
 region = $AWS_REGION
 EOF
